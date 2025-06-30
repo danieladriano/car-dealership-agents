9b6a6a15c93a3e34627cc9547e721bd011e21e67
Daniel Adriano <daniel.dadriano@gmail.com>
2025-06-30 23:46:18 +0100

    Added cognee pipeline and search

---
+++ cognee_pipeline.py
@@ -0,0 +1,53 @@
+from argparse import ArgumentParser
+import os
+import asyncio
+from pathlib import Path
+from cognee import config, prune, add, cognify, visualize_graph
+
+
+def config_cognee() -> None:
+    data_directory_path = str(
+        Path(
+            os.path.join(Path(__file__).parent, ".data_storage")
+        ).resolve()
+    )
+
+    cognee_directory_path = str(
+        Path(
+            os.path.join(Path(__file__).parent, ".cognee_system")
+        ).resolve()
+    )
+
+    config.data_root_directory(data_directory_path)
+    config.system_root_directory(cognee_directory_path)
+
+async def prune_cognee() -> None:
+    await prune.prune_data()
+    await prune.prune_system(metadata=True)
+
+async def main(assets_path: Path) -> None:
+    config_cognee()
+    await prune_cognee()
+
+    for file_path in assets_path.iterdir():
+        with open(file_path, "r") as file:
+            file_content = file.read()
+            if file_content:
+                await add(file_content)
+    
+    await cognify()
+
+    graph_file_path = str(
+        Path(
+            os.path.join(Path(__file__).parent, ".artifacts/graph_visualization.html")
+        ).resolve()
+    )
+    await visualize_graph(graph_file_path)
+
+
+if __name__ == "__main__":
+    argument_parser = ArgumentParser()
+    argument_parser.add_argument("--files", "-f")
+    args = argument_parser.parse_args()
+
+    asyncio.run(main(assets_path=Path(args.files)))
---
+++ cognee_search.py
@@ -0,0 +1,50 @@
+import os
+from pathlib import Path
+import streamlit as st
+import asyncio
+import cognee
+import logging
+
+logger = logging.getLogger(__name__)
+
+def load_cognee() -> None:
+    data_path = str(Path(  # noqa: F821
+            os.path.join(Path(__file__).parent, ".data_storage")
+        ).resolve()
+    )
+
+    cognee_path = str(
+        Path(os.path.join(Path(__file__).parent, ".cognee_system")).resolve()
+    )
+
+    cognee.config.data_root_directory(data_root_directory=data_path)
+    cognee.config.system_root_directory(system_root_directory=cognee_path)
+
+
+async def search(query_text: str) -> str:
+    logger.info(f"Calling cognee: {query_text}")
+    response = await cognee.search(query_text=query_text, query_type=cognee.SearchType.RAG_COMPLETION)
+    logger.info(f"Response: {response}")
+    return response[0]
+
+st.title("Simple chat")
+
+load_cognee()
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(body=prompt)
+    
+    with st.chat_message("assistant"):
+        response = asyncio.run(search(query_text=prompt))
+        st.markdown(response)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
