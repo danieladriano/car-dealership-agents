2f91da0f8e503cf972f2e4feb05fa26471228d1a
Daniel Adriano <daniel.dadriano@gmail.com>
2025-07-02 07:33:26 +0100

    Added car information

--- tools/sales.py
+++ tools/sales.py
@@ -1,8 +1,10 @@
 import logging
+from typing import Optional
 
+from click import Option
 from langchain_core.tools import tool
 
-from store.dealership_store import INVENTORY, Car
+from store.dealership_store import INVENTORY, Car, Models
 
 logger = logging.getLogger("ai-chat")
 
@@ -16,3 +18,22 @@ def list_inventory() -> list[Car]:
     """
     logger.info("Getting inventory")
     return INVENTORY.availables
+
+
+@tool
+def car_information(model: Models, year: int) -> Optional[Car]:
+    """Get more information about specific car
+
+    Args:
+        model (Models): Car model to get information
+        year (int): Car year to get information
+
+    Returns:
+        Optional[Car]: The car that the user wants more information
+    """
+    for car in INVENTORY.availables:
+        if car.model == model and car.year == year:
+            logger.info(f"Car information {car}")
+            return car
+    logger.info("The car was not found")
+    return None
